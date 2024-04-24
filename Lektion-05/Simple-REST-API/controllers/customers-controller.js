4ccdafd2919d56db29d586badb6ac280c38223e6
MichaelGustavsson <michael.gustavsson@skaftoskorpan.se>
2024-04-24 12:12:49 +0200

    Partial update customers-controller.js

--- Lektion-05/Simple-REST-API/controllers/customers-controller.js
+++ Lektion-05/Simple-REST-API/controllers/customers-controller.js
@@ -19,21 +19,16 @@ module.exports.listCustomers = (req, res, next) => {
 
 module.exports.findCustomer = (req, res, next) => {
   try {
-    const customer = customers.find((c) => c.id === req.params.id);
+    // const customer = customers.find((c) => c.id === req.params.id);
+    const customer = getCustomer(req, res, next);
 
-    if (!customer) {
-      return next(
-        new ErrorResponse(
-          `Kunde inte hitta någon kund med id: ${req.params.id}`
-        )
-      );
+    if (customer) {
+      res
+        .status(200)
+        .json(new ResponseModel({ statusCode: 200, data: customer }));
     }
-
-    res
-      .status(200)
-      .json(new ResponseModel({ statusCode: 200, data: customer }));
   } catch (error) {
-    next(new ErrorResponse('FindCustomer funkar inte!', 500));
+    next(new ErrorResponse(error.message, 500));
   }
 };
 
@@ -56,34 +51,27 @@ module.exports.addCustomer = (req, res, next) => {
 
 module.exports.updateCustomer = (req, res, next) => {
   try {
-    const customer = customers.find((c) => c.id === req.params.id);
+    const customer = getCustomer(req, res, next);
 
-    if (!customer) {
-      return next(
-        new ErrorResponse(
-          `Kunde inte hitta någon kund med id: ${req.params.id}`
-        )
-      );
+    if (customer) {
+      customer.name = req.body.name ?? customer.name;
+      customer.phone = req.body.phone ?? customer.phone;
+      customer.email = req.body.email ?? customer.email;
+      customer.contactPerson = req.body.contactPerson ?? customer.contactPerson;
+      customer.comment = req.body.comment ?? customer.comment;
+      customer.active = req.body.active ?? customer.active;
+
+      if (req.body.address) {
+        customer.address.street =
+          req.body.address.street ?? customer.address.street;
+        customer.address.postalCode =
+          req.body.address.postalCode ?? customer.address.postalCode;
+        customer.address.city = req.body.address.city ?? customer.address.city;
+      }
+
+      fileHandler(folder, file, customers);
+      res.status(204).end();
     }
-
-    customer.name = req.body.name ?? customer.name;
-    customer.phone = req.body.phone ?? customer.phone;
-    customer.email = req.body.email ?? customer.email;
-    customer.contactPerson = req.body.contactPerson ?? customer.contactPerson;
-    customer.comment = req.body.comment ?? customer.comment;
-    customer.active = req.body.active ?? customer.active;
-
-    if (req.body.address) {
-      customer.address.street =
-        req.body.address.street ?? customer.address.street;
-      customer.address.postalCode =
-        req.body.address.postalCode ?? customer.address.postalCode;
-      customer.address.city = req.body.address.city ?? customer.address.city;
-    }
-
-    fileHandler(folder, file, customers);
-
-    res.status(204).end();
   } catch (error) {
     next(new ErrorResponse(error.message, 500));
   }
@@ -129,4 +117,16 @@ module.exports.deleteCustomer = (req, res, next) => {
   } catch (error) {
     next(new ErrorResponse(error.message, 500));
   }
+};
+
+const getCustomer = (req, res, next) => {
+  const customer = customers.find((c) => c.id === req.params.id);
+
+  if (!customer) {
+    return next(
+      new ErrorResponse(`Kunde inte hitta någon kund med id: ${req.params.id}`)
+    );
+  }
+
+  return customer;
 };
